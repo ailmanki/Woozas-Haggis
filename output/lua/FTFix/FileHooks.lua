89f74929c9e46bc15c62ac380b3be1a484679a23
diego <1570625+ailmanki@users.noreply.github.com>
2021-09-04 11:43:30 +0200

    add flamethrower fix

---
+++ output/lua/FTFix/FileHooks.lua
@@ -0,0 +1,7 @@
+local function ModLoader_SetupFileHook(file, replace_type)
+    local FTFix_file = string.gsub(file, "lua/", "lua/FTFix/", 1)
+
+    ModLoader.SetupFileHook(file,  FTFix_file, replace_type)
+end
+
+ModLoader_SetupFileHook( "lua/Weapons/Marine/Flamethrower.lua", "post" )
